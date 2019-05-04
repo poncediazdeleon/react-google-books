29277bb327e3d0b29eda39bba4f327c846e00e11
Michael Diaz <poncediazdeleon@gmail.com>
2019-05-03 18:56:47 -0600

    upload

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+function Logo() {
+    return (
+            <div className="logo">
+                <img className="logo-position" src="http://www.cliparthut.com/clip-arts/201/alcohol-use-headstrong-clipart-dvYTUV.png" />
+                <h1 className="logo-text logo-position"> Google Books Search </h1>
+            </div>
+    )
+}
+
+export default Logo;
